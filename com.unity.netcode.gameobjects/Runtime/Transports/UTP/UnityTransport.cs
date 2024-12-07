28e9ea169c32a5a1d8d2379fafe2069b8964d846
Boyne <54646090+DedFishy@users.noreply.github.com>
2024-12-07 13:46:51 -0500

    Update UnityTransport.cs

--- com.unity.netcode.gameobjects/Runtime/Transports/UTP/UnityTransport.cs
+++ com.unity.netcode.gameobjects/Runtime/Transports/UTP/UnityTransport.cs
@@ -1543,13 +1543,6 @@ namespace Unity.Netcode.Transports.UTP
 #endif
                 heartbeatTimeoutMS: transport.m_HeartbeatTimeoutMS);
 
-#if UNITY_WEBGL && !UNITY_EDITOR
-            if (NetworkManager.IsServer && m_ProtocolType != ProtocolType.RelayUnityTransport)
-            {
-                throw new Exception("WebGL as a server is not supported by Unity Transport, outside the Editor.");
-            }
-#endif
-
 #if UTP_TRANSPORT_2_0_ABOVE
             if (m_UseEncryption)
             {
